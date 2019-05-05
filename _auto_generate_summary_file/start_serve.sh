97b520b48448ef70974a36ce5f92c5639214b92b
guopan <1144290241@qq.com>
2019-05-05 16:36:18 +0800

    增加mr里特殊字符的处理方案

--- _auto_generate_summary_file/start_serve.sh
+++ _auto_generate_summary_file/start_serve.sh
@@ -20,7 +20,7 @@ echo 'gitbook studynote: autogenerate success'
 
 # 启动服务
 cd ..
-nohup gitbook serve --port 3000 --lrport 35728 > gitbook_serve_log.log 2>&1 & 
+nohup gitbook serve --port 3000 --lrport 35728 > _auto_generate_summary_file/gitbook_serve_log.log 2>&1 & 
 # gitbook serve --port 3000 --lrport 35728
 echo 'gitbook studynote: gitbook serve success at http://localhost:3000/'
 
